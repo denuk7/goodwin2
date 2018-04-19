3d9e3aca139f1c3f02f245478b8bbe526c054de6
denuk <khmnkd@gmail.com>
2018-04-19 18:22:10 +0300

    added vue js

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,25 +9,17 @@
 namespace App\Controller;
 
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class MainController extends AbstractController
 {
   /**
-   * @Route("/news/{slug}", name="article_show")
+   * @Route("/test", name="test")
    */
-  public function show($slug)
+  public function test()
   {
-    $comments = [
-      'I ate a normal rock once. It did NOT taste like bacon!',
-      'Woohoo! I\'m going on an all-asteroid diet!',
-      'I like bacon too! Buy some from my site! bakinsomebacon.com',
-    ];
-
-    return $this->render('article/show.html.twig', [
-      'title' => ucwords(str_replace('-', ' ', $slug)),
-      'comments' => $comments,
-      'slug' => $slug,
-    ]);
+    return new JsonResponse('TEST');
   }
 }
